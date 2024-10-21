4728d3b3bad9ed0fcb08615f34ffda46d8d0564c
memoTux <romeo@mendezfuentes.net>
2024-10-20 21:41:38 -0600

    feat(components): Plot

---
+++ src/components/Plot.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { plot, type PlotOptions, type Markish } from '@observablehq/plot'
+import { onMounted, onUpdated, useTemplateRef } from 'vue'
+
+const props = defineProps<{
+  options?: PlotOptions
+  marks?: Markish[]
+  defer?: Boolean
+}>()
+
+const options: PlotOptions = {
+  marks: props.marks == null ? [] : [props.marks],
+  width: 688, // better default for VitePress
+  className: 'plot',
+  ...props.options,
+}
+
+const el = useTemplateRef<HTMLDivElement>('container')
+
+const replace = () => {
+  if (!el.value) return
+
+  el.value.replaceChildren(plot(options))
+}
+
+onMounted(replace)
+onUpdated(replace)
+</script>
+
+<template>
+  <div
+    class="plot-container"
+    ref="container"
+  >
+    <h1>Plot</h1>
+  </div>
+</template>
