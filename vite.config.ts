7628fb2c666752d3247bb6395f542e675de82eb7
memoTux <romeo@mendezfuentes.net>
2024-11-08 09:46:28 -0600

    chore: clean up tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 /// <reference types="vitest/config" />
 
-import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -9,27 +8,5 @@ export default defineConfig({
   plugins: [vue()],
   test: {
     environment: 'happy-dom'
-  },
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/main.ts'),
-      name: 'Vue Plot',
-      // the proper extensions will be added
-      fileName: 'vue-plot',
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue', '@observablehq/plot'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-          '@observablehq/plot': '@observablehq/plot'
-        },
-      },
-    },
-  },
+  }
 })
