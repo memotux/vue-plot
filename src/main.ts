90e238f6317502d5e82f0e500d380c3cfe307045
memoTux <romeo@mendezfuentes.net>
2024-11-08 09:18:58 -0600

    chore: add vue plugin install function

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
+import type { App } from "vue";
 import Plot from "./components/Plot.vue";
 import { usePlot } from "./composable/plot.ts";
 
-export { Plot, usePlot }
+/**
+ * Vue Plugin install function
+ * @param app { App } Vue App context
+ */
+function VuePlot(app: App) {
+  app.component('Plot', Plot)
+}
+
+export { Plot, usePlot, VuePlot }
