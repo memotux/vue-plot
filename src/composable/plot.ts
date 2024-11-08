ac113fbebe1b0fcc8fb05a38d7c550daee60abb7
memoTux <romeo@mendezfuentes.net>
2024-11-08 09:42:58 -0600

    chore: composable plot docs

--- src/composable/plot.ts
+++ src/composable/plot.ts
@@ -1,6 +1,11 @@
 import { plot, type PlotOptions } from '@observablehq/plot'
 import { type Ref, onMounted, onUpdated, useTemplateRef } from 'vue'
 
+/**
+ * 
+ * @param options { Ref<PlotOptions> } Vue ref PlotOptions
+ * @param key { string } Template Ref key
+ */
 export function usePlot<E extends HTMLElement>(options: Ref<PlotOptions>, key: string) {
 
   const el = useTemplateRef<E>(key)
