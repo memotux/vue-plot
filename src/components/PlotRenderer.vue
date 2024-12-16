8216661e47fb5e17e8b9226a514d7cf29f9d25c8
memoTux <romeo@mendezfuentes.net>
2024-12-16 14:42:27 -0600

    fix: Vue warn 'slot invoked outside render function'

--- src/components/PlotRenderer.vue
+++ src/components/PlotRenderer.vue
@@ -1,27 +1,20 @@
 <script lang="ts" setup>
-import {
-  h,
-  onMounted,
-  useTemplateRef,
-  createRenderer,
-  defineComponent
-} from 'vue'
+import { h, onMounted, useTemplateRef, createRenderer } from 'vue'
 import { nodeOps } from '../core'
 import type { PlotOptions } from '@observablehq/plot'
+import type { VNode } from 'vue'
 
 const props = withDefaults(defineProps<PlotOptions>(), {
   aspectRatio: null,
 })
 const slots = defineSlots<{
-  default?: () => any
+  default?: () => VNode
 }>()
 defineOptions({
   inheritAttrs: false,
 })
 
-const InternalComponent = defineComponent({
-  setup: () => () => h('PlotRoot', {...props}, slots.default?.() || []),
-})
+const InternalComponent = () => h('PlotRoot', { ...props }, () => slots.default?.() || [])
 
 const plotRoot = useTemplateRef('plot-root')
 const { render } = createRenderer(nodeOps)
