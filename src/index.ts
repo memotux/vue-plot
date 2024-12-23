e0e39d3e9fde30c14c2dabd8602e0b5714ecf924
memoTux <romeo@mendezfuentes.net>
2024-12-23 08:58:34 -0600

    chore: add vite build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import type { App } from "vue";
-import Plot from "./components/Plot.vue";
+import { Plot } from "./components";
 import { plotCustomElement } from "./core";
 
 /**
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,53 @@
 /// <reference types="vitest/config" />
 
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import vue from '@vitejs/plugin-vue'
-import { plotCustomElement } from "./src/core";
+import { resolve } from 'pathe'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    ...plotCustomElement
-  })],
+  plugins: [
+    vue({
+      isProduction: false,
+      template: {
+        compilerOptions: {
+          isCustomElement: tag => tag.startsWith('Plot'),
+        },
+      },
+    }),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  // @ts-ignore
   test: {
-    environment: 'happy-dom'
-  }
+    environment: 'happy-dom',
+    globals: true,
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vue-plot',
+      fileName: 'vue-plot',
+    },
+    watch: {
+      include: [resolve(__dirname, 'src')],
+    },
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ['vue', '@observablehq/plot'],
+      output: {
+        exports: 'named',
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          'vue': 'Vue',
+          '@observablehq/plot': 'Plot',
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    exclude: ['vue', '@observablehq/plot'],
+  },
 })
