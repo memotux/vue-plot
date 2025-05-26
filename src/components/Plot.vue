c824c5951a18153732e23437832f16bc06b694a7
memoTux <romeo@mendezfuentes.net>
2025-05-26 16:16:00 -0600

    refactor: Plot Internal component

--- src/components/Plot.vue
+++ src/components/Plot.vue
@@ -1,12 +1,5 @@
 <script setup lang="ts">
-import {
-  h,
-  onMounted,
-  useSlots,
-  useTemplateRef,
-  createRenderer,
-  defineComponent,
-} from 'vue'
+import { h, onMounted, useSlots, useTemplateRef, createRenderer } from 'vue'
 import { nodeOps, createPlotContext } from '../core'
 import type { PlotOptions } from '@observablehq/plot'
 
@@ -16,34 +9,31 @@ const props = withDefaults(defineProps<PlotOptions>(), {
 })
 const slots = useSlots()
 
-const InternalComponent = defineComponent({
-  setup() {
-    /**
-     * [Vue warn]: Slot "default" invoked outside of the render function: this will not
-     * track dependencies used in the slot. Invoke the slot function inside the render
-     * function instead.
-     */
-    if (!slots.default && !Boolean(props.marks)) {
-      console.warn('Please add Plot Marks in props or as children')
-      return () =>
-        h('PlotRoot', null, [
-          h('PlotFrame'),
-          h('PlotText', {
-            data: ['Please add Plot Marks in props or as children'],
-            frameAnchor: 'middle',
-          }),
-        ])
-    }
-    return () => h('PlotRoot', props, slots.default?.())
-  },
-})
-
 const plotContainer = useTemplateRef('plot-container')
+
+const PlotInternal = () => {
+  /**
+   * [Vue warn]: Slot "default" invoked outside of the render function: this will not
+   * track dependencies used in the slot. Invoke the slot function inside the render
+   * function instead.
+   */
+  if (!slots.default && !Boolean(props.marks)) {
+    console.warn('Please add Plot Marks in props or as children')
+    return h('PlotRoot', null, [
+      h('PlotFrame'),
+      h('PlotText', {
+        data: ['Please add Plot Marks in props or as children'],
+        frameAnchor: 'middle',
+      }),
+    ])
+  }
+  return h('PlotRoot', props, slots.default?.())
+}
 
 onMounted(() => {
   const ctx = createPlotContext(plotContainer.value)
   const { render } = createRenderer(nodeOps(ctx))
-  render(h(InternalComponent), plotContainer.value)
+  render(h(PlotInternal), plotContainer.value)
 })
 </script>
 
