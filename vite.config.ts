6558d1daec9857342ecbfe89fe8479ae10833673
memoTux <romeo@mendezfuentes.net>
2026-05-05 14:45:21 -0600

    chore(deps): vite 8, unplugin-dts 1
    - Upgrade vite from rolldown-vite@7 to vite@8
    - Replace vite-plugin-dts with unplugin-dts
    - Exclude .atl/ directory from git

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest/config" />
 
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
+import dts from 'unplugin-dts/vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'pathe'
 
